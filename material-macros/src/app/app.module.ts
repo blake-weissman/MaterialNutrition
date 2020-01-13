eca77da04d67cfbdf1f95fa47d89085fc2c0ab44
BlakeWeissman <blake.r.weissman@gmail.com>
2020-01-13 11:10:55 -0500

    Created basic project.

---
+++ material-macros/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ material-macros/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyCxv8H_gopvTbCPDRdxhc_DQHFsZDGtZAk",
+    authDomain: "material-macros-v1.firebaseapp.com",
+    databaseURL: "https://material-macros-v1.firebaseio.com",
+    projectId: "material-macros-v1",
+    storageBucket: "material-macros-v1.appspot.com",
+    messagingSenderId: "464039780416",
+    appId: "1:464039780416:web:5b16650341fa94d02f7fa5",
+    measurementId: "G-8KBKVQPVLH"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
