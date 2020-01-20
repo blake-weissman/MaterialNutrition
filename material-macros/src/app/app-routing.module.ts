376fa4e1290ed831b993a0deb3b05301a6372c39
BlakeWeissman <blake.r.weissman@gmail.com>
2020-01-20 11:42:32 -0500

    Created basic routes for proper login redirects.

--- material-macros/src/app/app-routing.module.ts
+++ material-macros/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+import { MainComponent } from './components/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginComponent,
+    ...canActivate(redirectLoggedInTo(['']))
+  },
+  { 
+    path: '',
+    component: MainComponent,
+    ...canActivate(redirectUnauthorizedTo(['login']))
+  },
+  // { 
+  //   path: '**', 
+  //   component: PageNotFoundComponent 
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
