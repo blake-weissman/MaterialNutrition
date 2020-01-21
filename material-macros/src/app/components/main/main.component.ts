f1930e7d0b6537b47262cba3106dabd10ef5a497
BlakeWeissman <blake.r.weissman@gmail.com>
2020-01-20 22:16:13 -0500

    Began implementation of displaying meal items with basic drag and drop implementation.

--- material-macros/src/app/components/main/main.component.ts
+++ material-macros/src/app/components/main/main.component.ts
@@ -8,17 +8,40 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 })
 export class MainComponent {
 
-  public movies = [
-    'One',
-    'Two',
-    'Three',
-    'Four',
-  ];
+  // TODO create proper global model/keys
+  // public OTHER_MEAL_ITEMS_KEY;
+  public meals = {
+    other: [
+      {
+        key: 'eggs',
+        quantity: 20,
+        type: 'food'
+      },
+      {
+        key: 'pizza',
+        quantity: 1,
+        type: 'recipie'
+      }
+    ],
+    breakfast: [
+      {
+        key: 'eggs',
+        quantity: 20,
+        type: 'food'
+      },
+      {
+        key: 'pizza',
+        quantity: 1,
+        type: 'recipie'
+      }
+    ]
+  };
 
   public drop(event: CdkDragDrop<string[]>) {
     // debugger;
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-    this.movies = [...this.movies];
+    console.log(event);
+    // moveItemInArray(this.meals[key], event.previousIndex, event.currentIndex);
+    // this.meals[key] = [...this.meals[key]];
   }
 
 }
