ab47306b31922e75ee067b2b77244e7e10bd437d
BlakeWeissman <blake.r.weissman@gmail.com>
2020-02-09 18:09:15 -0500

    Created basic meals data connection by listening to router changes.

--- material-macros/src/app/components/main/main.component.ts
+++ material-macros/src/app/components/main/main.component.ts
@@ -1,132 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+enum Type {
+  FOOD,
+  RECIPIE
+}
+
+interface Meals {
+  name: string, 
+  items: {
+    key: string, 
+    servings: number, 
+    type: Type, 
+    units: string
+  }[]
+}
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
+  private dateSubscription: Subscription;
+  public meals: Meals[];
 
   constructor(
     private router: Router,
+    private activatedRoute: ActivatedRoute
     ) {
+
+  }
+
+  ngOnInit() {
+    this.dateSubscription = this.activatedRoute.params.subscribe(params => {
+      this.meals = this.days[params.date];
+      // console.log(params['date']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.dateSubscription.unsubscribe();
   }
 
   onDateSelect(date: Date): void {
     this.router.navigateByUrl('/' + date.getTime());
   } 
   
-  public days = {
-
+  public days: {
+    [key: number]: Meals[]
+  } = {
+    1581224400000: [
+      {
+        name: 'Other',
+        items: [
+          {
+            key: 'eggs',
+            servings: 20,
+            type: Type.FOOD,
+            units: 'grams',
+          },
+          {
+            key: 'eggs',
+            servings: 20,
+            type: Type.FOOD,
+            units: 'grams',
+          },
+        ]
+      }
+    ]
   }
 
-  // TODO create proper global model/keys
-  public meals = [
-    {
-      name: 'Other',
-      mealItems: [
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        {
-          key: 'eggs',
-          servings: 20,
-          type: 'food',
-          units: 'grams',
-        },
-        // {
-        //   key: 'pizza',
-        //   servings: 1,
-        //   type: 'recipie',
-        //   units: 'grams',
-        // }
-      ]
-    },
-    // {
-    //   name: 'Breakfast',
-    //   mealItems: [
-    //     {
-    //       key: 'eggs',
-    //       servings: 20,
-    //       type: 'food',
-    //       units: 'grams',
-    //     },
-    //     // {
-    //     //   key: 'pizza',
-    //     //   servings: 1,
-    //     //   type: 'recipie',
-    //     //   units: 'grams',
-    //     // }
-    //   ]
-    // },
-    // {
-    //   name: 'Lunch',
-    //   mealItems: [
-    //     {
-    //       key: 'eggs',
-    //       servings: 20,
-    //       type: 'food',
-    //       units: 'grams',
-    //     },
-    //     // {
-    //     //   key: 'pizza',
-    //     //   servings: 1,
-    //     //   type: 'recipie',
-    //     //   units: 'grams',
-    //     // }
-    //   ]
-    // },
-    // {
-    //   name: 'Dinner',
-    //   mealItems: [
-    //     {
-    //       key: 'eggs',
-    //       servings: 20,
-    //       type: 'food',
-    //       units: 'grams',
-    //     },
-    //     // {
-    //     //   key: 'pizza',
-    //     //   servings: 1,
-    //     //   type: 'recipie',
-    //     //   units: 'grams',
-    //     // }
-    //   ]
-    // },
-  ]
-
-  public drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.meals, event.previousIndex, event.currentIndex);
-    this.meals = [...this.meals];
-  }
+  // public drop(event: CdkDragDrop<string[]>) {
+  //   moveItemInArray(this.meals, event.previousIndex, event.currentIndex);
+  //   this.meals = [...this.meals];
+  // }
 }
