bcd5a9454e762ae896a1247774d6145bcb656273
BlakeWeissman <blake.r.weissman@gmail.com>
2020-02-18 21:49:52 -0500

    Cleaned up code.

--- material-macros/src/app/app.module.ts
+++ material-macros/src/app/app.module.ts
@@ -34,25 +34,6 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
   signInFlow: 'popup',
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // {
-    //   scopes: [
-    //     'public_profile',
-    //     'email',
-    //     'user_likes',
-    //     'user_friends'
-    //   ],
-    //   customParameters: {
-    //     'auth_type': 'reauthenticate'
-    //   },
-    //   provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    // },
-    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    // {
-    //   requireDisplayName: false,
-    //   provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
-    // },
-    // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   ],
   tosUrl: '<your-tos-link>',
