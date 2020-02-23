25cf51f5e02171109e16841ba7ad9cb1215c1b36
BlakeWeissman <blake.r.weissman@gmail.com>
2020-02-23 15:13:29 -0500

    Created basic initial layout/style for the itemsDialog.

--- material-macros/src/app/components/track/items/items.component.ts
+++ material-macros/src/app/components/track/items/items.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { UserItemType } from 'src/app/model/items';
+import { UserService } from 'src/app/services/user/user.service';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss']
 })
-export class ItemsDialogComponent {}
+export class ItemsDialogComponent {
+  // public userItems = this.userService.user.items;
+  public UserItemType = UserItemType;
+
+  constructor(public userService: UserService) {
+  }
+
+  public filterItems(event) {
+    event.stopPropagation();
+    console.log(event);
+  }
+}
 
 @Component({
   template: '',
