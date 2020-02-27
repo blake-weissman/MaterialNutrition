15db1c8aace15b02a0f3787038ae7f282936eb10
BlakeWeissman <blake.r.weissman@gmail.com>
2020-02-26 21:39:43 -0500

    Created basic two-way binding to a new userFoodItem.

--- material-macros/src/app/components/track/items/create-item/create-item.component.ts
+++ material-macros/src/app/components/track/items/create-item/create-item.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserFoodItem } from 'src/app/model/items';
 
 @Component({
   selector: 'app-create-item',
   templateUrl: './create-item.component.html',
   styleUrls: ['./create-item.component.scss']
 })
-export class CreateItemComponent {}
+export class CreateItemComponent {
+  public userFoodItem: UserFoodItem = new UserFoodItem(); 
+}
