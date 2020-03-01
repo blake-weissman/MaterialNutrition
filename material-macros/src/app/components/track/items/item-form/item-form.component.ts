871da8493b7f2aa57ef1526a1033f6bda06330c5
BlakeWeissman <blake.r.weissman@gmail.com>
2020-02-29 21:47:17 -0500

    Added "formInvalidityChanged" event emitter.

--- material-macros/src/app/components/track/items/item-form/item-form.component.ts
+++ material-macros/src/app/components/track/items/item-form/item-form.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
 import { UserFoodItem } from 'src/app/model/items';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-item-form',
   templateUrl: './item-form.component.html',
   styleUrls: ['./item-form.component.scss']
 })
-export class ItemFormComponent {
+export class ItemFormComponent implements AfterViewInit {
+  @ViewChild('form', {static: false}) private form: NgForm;
   @Input() public userFoodItem: UserFoodItem;
+  @Output() private formInvalidityChanged = new EventEmitter<boolean>();
+
+  ngAfterViewInit() {
+    this.form.valueChanges.subscribe(() => {
+      this.formInvalidityChanged.emit(this.form.invalid);
+    })
+  }
 }
