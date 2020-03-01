7d69c1958a5d5bd37af100745ef8f28e87f2ae34
BlakeWeissman <blake.r.weissman@gmail.com>
2020-03-01 12:37:12 -0500

    Implemented deepCopy when selecting a userItem.

--- material-macros/src/app/components/track/user-items-dialog/edit-user-items/edit-user-items.component.ts
+++ material-macros/src/app/components/track/user-items-dialog/edit-user-items/edit-user-items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
 import { UserItemType, UserFoodItem, UserRecipeItem } from 'src/app/model/items';
+import { AppService } from 'src/app/services/app/app.service';
 
 @Component({
   selector: 'app-edit-user-items',
@@ -13,7 +14,10 @@ export class EditUserItemsComponent {
   public selectedUserFoodItem: UserFoodItem;
   public selectedUserRecipeItem: UserRecipeItem;
   
-  constructor(public userService: UserService) {}
+  constructor(
+    public userService: UserService,
+    public appService: AppService
+  ) {}
 
   public filterItems(event) {
     event.stopPropagation();
@@ -22,9 +26,11 @@ export class EditUserItemsComponent {
 
   public selectUserItem(item: UserFoodItem | UserRecipeItem, type: UserItemType) {
     if (type === UserItemType.FOOD) {
-      this.selectedUserFoodItem = item as UserFoodItem;
+      this.selectedUserFoodItem = this.appService.deepCopy<UserFoodItem>(item as UserFoodItem);
+      this.selectedUserRecipeItem = null;
     } else {
-      this.selectedUserRecipeItem = item as UserRecipeItem;
+      this.selectedUserRecipeItem = this.appService.deepCopy<UserRecipeItem>(item as UserRecipeItem);
+      this.selectedUserFoodItem = null;
     }
   }
 }
