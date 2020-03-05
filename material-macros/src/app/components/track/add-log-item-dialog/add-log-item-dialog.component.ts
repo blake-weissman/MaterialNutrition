18faf7ec08a05a93ce62699ddfa525a9a1a4d2f3
BlakeWeissman <blake.r.weissman@gmail.com>
2020-03-04 22:14:10 -0500

    Users can now add a food item in the add-log-item-dialog.

--- material-macros/src/app/components/track/add-log-item-dialog/add-log-item-dialog.component.ts
+++ material-macros/src/app/components/track/add-log-item-dialog/add-log-item-dialog.component.ts
@@ -2,13 +2,37 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
+import { UserItem, UserItemType } from 'src/app/model/items';
+import { UserService } from 'src/app/services/user/user.service';
+import { AppService } from 'src/app/services/app/app.service';
 
 @Component({
   selector: 'app-add-log-item-dialog',
   templateUrl: './add-log-item-dialog.component.html',
   styleUrls: ['./add-log-item-dialog.component.scss']
 })
-export class AddLogItemDialogComponent {}
+export class AddLogItemDialogComponent {
+  private selectedUserItem: UserItem;
+  
+  constructor (
+    private userService: UserService,
+    private appService: AppService,
+  ) {}
+
+  public userItemSelected(userItem: UserItem, userItemType: UserItemType): void {
+    this.selectedUserItem = userItem;
+  }
+
+  public addNewLogItem(): void {
+    const selectedEpochLog = this.userService.user.log[this.userService.selectedEpoch];
+    this.userService.getUserFirestoreDocument().update(this.appService.convertCustomObjectToObject({
+      log: {
+        ...this.userService.user.log,
+        [this.userService.selectedEpoch]: [...(selectedEpochLog ? selectedEpochLog : []), this.selectedUserItem]
+      }
+    }))
+  }
+}
 
 @Component({
   template: '',
