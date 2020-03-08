7e79e839aa21bd10f6525cdf88a68c9f120a8c0e
BlakeWeissman <blake.r.weissman@gmail.com>
2020-03-08 14:56:00 -0400

    Fleshed out template functionality.

--- material-macros/src/app/components/track/goals-dialog/goals-dialog.component.ts
+++ material-macros/src/app/components/track/goals-dialog/goals-dialog.component.ts
@@ -2,13 +2,24 @@ import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { NutritionData, NutritionDataKeys } from 'src/app/model/items';
+import { UserService } from 'src/app/services/user/user.service';
+import { AppService } from 'src/app/services/app/app.service';
 
 @Component({
   selector: 'app-goals-dialog',
   templateUrl: './goals-dialog.component.html',
   styleUrls: ['./goals-dialog.component.scss']
 })
-export class GoalsDialogComponent {}
+export class GoalsDialogComponent {
+  public currentUserGoals: NutritionData = this.appService.deepCopy(this.userService.user.goals);
+  public NutritionDataKeys = NutritionDataKeys;
+
+  constructor(
+    private userService: UserService,
+    private appService: AppService,
+  ) {}
+}
 
 @Component({
   template: '',
@@ -21,7 +32,7 @@ export class GoalsDialogEntryComponent implements OnDestroy {
     private router: Router,
   ) {
     const itemsDialog = this.matDialog.open(GoalsDialogComponent, {
-      maxWidth: '400px',
+      maxWidth: '275px',
     });
     this.redirectWhenClosedSubscription = itemsDialog.afterClosed().subscribe(() => {
       this.router.navigate(['../']);
