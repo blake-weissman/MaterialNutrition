a1838a9774b0943ffad72cce7630dadf3be8a0c2
BlakeWeissman <blake.r.weissman@gmail.com>
2020-03-19 22:15:39 -0400

    Full changed project name to "material nutrition"

--- material-macros/src/app/app.component.spec.ts
+++ material-macros/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'material-macros'`, () => {
+  it(`should have as title 'material-nutrition'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('material-macros');
+    expect(app.title).toEqual('material-nutrition');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('material-macros app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('material-nutrition app is running!');
   });
 });
