0ce7631c9ba7420a1cb0d9be3d142de8a30c28b3
BlakeWeissman <blake.r.weissman@gmail.com>
2020-03-20 20:58:21 -0400

    Minor routing fixes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo, AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { TrackComponent } from './components/track/track.component';
 import { UserItemsDialogEntryComponent } from './components/track/user-items-dialog/user-items-dialog.component';
 import { CreateUserItemComponent } from './components/track/user-items-dialog/create-user-item/create-user-item.component';
 import { EditUserItemsComponent } from './components/track/user-items-dialog/edit-user-items/edit-user-items.component';
 import { AddLogItemDialogEntryComponent } from './components/track/add-log-item-dialog/add-log-item-dialog.component';
 import { GoalsDialogEntryComponent } from './components/track/goals-dialog/goals-dialog.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   { 
@@ -37,8 +39,11 @@ const routes: Routes = [
   { 
     path: '',
     pathMatch: 'full',
+    canActivate: [AngularFireAuthGuard],
+    data: { 
+      authGuardPipe: redirectUnauthorizedToLogin 
+    },
     component: TrackComponent,
-    ...canActivate(redirectUnauthorizedTo(['login']))
   },
   { 
     path: '**', 
