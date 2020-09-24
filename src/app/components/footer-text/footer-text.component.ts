ee240346234294dc9125de6650e7e4cf914da344
Blake Weissman <contact@blakeweissman.com>
2020-09-24 09:54:40 -0400

    Imporved firebaseui mobile style

--- src/app/components/footer-text/footer-text.component.ts
+++ src/app/components/footer-text/footer-text.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FooterTextComponent {
   @Input() public multiline: boolean;
   public currentDate = new Date();
-  public version = "v0.2";
+  public version = "v0.2 Beta";
 
   constructor() { }
 }
